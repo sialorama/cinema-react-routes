c36440b731d651a486d0e44a35b0c4fc4788fd21
29010-55-13 <29010-55-13@exchange.ad.afpanet>
2024-08-23 11:53:03 +0200

    update home link data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const Home = () => {
+function HomeList() {
+    const [home, setHome] = useState([]);
+
+    useEffect(() => {
+                // Fetch list of Home from cinema DB
+                axios
+                .get(`https://localhost:8088/`)
+                .then((response) => {
+                    setHome(response.data); // Set the film data
+                })
+                .catch((error) => {
+                    console.error("Error fetching data:", error);
+                });
+        });
+
     return (
-        <div>Home</div>
-    )
+        <div>
+            <h1>Films Populaires</h1>
+            <div style={{ display: "flex", flexWrap: "wrap" }}>
+                {home.map((film) => (
+                    <div key={film.id} style={{ margin: "20px" }}>
+                        <h3>{film.titre}</h3>
+
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default Home
+export default HomeList;
